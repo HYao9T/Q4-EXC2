8afdb26f832355dd6a311ffa1103aa0d79728db1
harhar <yao.hgk@obmontessori.edu.ph>
2025-03-25 11:16:13 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+// // IF STATEMENT
+// function verifyAge(){
+// 	let age = document.getElementById('input_age').value;
+
+// 	if (age <= 17){
+// 		window.alert("You're too young to vote!");		
+// 	}
+// }
+
+
+
+
+function verifyAge(){
+	let age = document.getElementById('input_age').value;
+
+	if (age <= 17){
+		window.alert("You're too young to vote!");		
+	}
+
+	else{
+		window.alert("You are eligible to vote!");
+	}
+}
